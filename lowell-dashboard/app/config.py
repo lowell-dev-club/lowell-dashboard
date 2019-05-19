8ba7bc04455c914bc223fea81b47845baf749a25
Savagecoder77 <savagecoder77@gmail.com>
2019-05-19 13:09:38 -0700

    create secrets file

--- lowell-dashboard/app/config.py
+++ lowell-dashboard/app/config.py
@@ -3,13 +3,15 @@ Put config materials here
 Ex: database configurations, keys, etc.
 '''
 
+import secret
+
 '''
 To create a secret key run the program
 import secrets
 secrets.token_hex(16)
 '''
 
-SECRET_KEY = '5791628bb0b13ce0c676dfde280ba245'
+SECRET_KEY = secret.SECRET_KEY
 SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
 
 '''
@@ -20,5 +22,5 @@ MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 465
 MAIL_USE_SSL = True
 MAIL_USERNAME = 'LowellHelpForum@gmail.com'
-# MAIL_PASSWORD = secret.EMAIL_PASS
-MAIL_DEFAULT_SENDER = 'LowellHelpForum <LowellHelpForum@gmail.com>'
+MAIL_PASSWORD = secret.EMAIL_PASS
+MAIL_DEFAULT_SENDER = 'Lowell Help Forum <LowellHelpForum@gmail.com>'
