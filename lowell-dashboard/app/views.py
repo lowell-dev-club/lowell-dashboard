3191951d1779f2e2f74a855a303d073f8cdade05
Savagecoder77 <savagecoder77@gmail.com>
2019-05-20 14:07:13 -0700

    run autopep8

--- lowell-dashboard/app/views.py
+++ lowell-dashboard/app/views.py
@@ -2,27 +2,32 @@ from app import app
 from flask import render_template, request, make_response, redirect, session, url_for, send_file, flash
 from app.forms import RegistrationForm, LoginForm
 
+
 @app.route("/")
 @app.route("/home")
 @app.route("/index")
 def home():
-    return render_template('home.html')	
+    return render_template('home.html')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-    	flash(f'Account created for {form.username.data}!', 'success')
-    	return redirect(url_for('home'))
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', form=form)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-    	if form.email.data == 'a@a.com' and form.password.data == 'pass':
-    		flash('You have been logged in!', 'success')
-    		return redirect(url_for('home'))
-    	else:
-    		flash('Login Unseccessful. Please check username and password', 'danger')
+        if form.email.data == 'a@a.com' and form.password.data == 'pass':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(
+                'Login Unseccessful. Please check username and password',
+                'danger')
     return render_template('login.html', form=form)
