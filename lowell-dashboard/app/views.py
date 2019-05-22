c138b4324e24242356e54a0a31e166d572727442
calee14 <caplee1000@gmail.com>
2019-05-22 08:32:14 -0700

    creating a User object and adding it to the db

--- lowell-dashboard/app/views.py
+++ lowell-dashboard/app/views.py
@@ -15,6 +15,10 @@ def home():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', form=form)
