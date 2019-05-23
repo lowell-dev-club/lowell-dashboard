9ca956f49b08ff191337fe386490e42194c54960
calee14 <caplee1000@gmail.com>
2019-05-22 20:55:45 -0700

    handling login and requiring login for some routes

--- lowell-dashboard/app/views.py
+++ lowell-dashboard/app/views.py
@@ -2,6 +2,7 @@ from app import app, db, bcrypt
 from flask import render_template, request, make_response, redirect, session, url_for, send_file, flash
 from app.forms import RegistrationForm, LoginForm
 from app.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 @app.route("/")
@@ -18,6 +19,8 @@ def license():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -31,13 +34,25 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'a@a.com' and form.password.data == 'pass':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash(
-                'Login Unseccessful. Please check username and password',
-                'danger')
+            flash('Login Unseccessful. Please check email and password', 'danger')
     return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/account')
+@login_required
+def account():
+    return render_template('account.html')
