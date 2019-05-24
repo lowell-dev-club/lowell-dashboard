0d19a5e6603c2e3875aa215a1b74bc554925b80c
Savagecoder77 <savagecoder77@gmail.com>
2019-05-23 21:33:00 -0700

    add double hash plus add salt

--- lowell-dashboard/app/views.py
+++ lowell-dashboard/app/views.py
@@ -1,5 +1,6 @@
 from app import app, db, bcrypt
 from flask import render_template, request, make_response, redirect, session, url_for, send_file, flash
+from hashlib import sha256
 from app.forms import RegistrationForm, LoginForm
 from app.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
@@ -23,7 +24,7 @@ def register():
         return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        hashed_password = bcrypt.generate_password_hash(sha256((form.password.data + form.username.data).encode()).hexdigest()).decode('utf-8')
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
@@ -39,7 +40,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
+        if user and bcrypt.check_password_hash(user.password, sha256((form.password.data + form.username.data).encode()).hexdigest()):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
             return redirect(next_page) if next_page else redirect(url_for('home'))
