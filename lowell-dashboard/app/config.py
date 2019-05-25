72f99b81f2607a8226acc6a073b4e58dc9d68994
Savagecoder77 <savagecoder77@gmail.com>
2019-05-24 23:46:36 -0700

    add sqlite back

--- lowell-dashboard/app/config.py
+++ lowell-dashboard/app/config.py
@@ -12,7 +12,7 @@ secrets.token_hex(16)
 '''
 
 SECRET_KEY = secret.SECRET_KEY
-SQLALCHEMY_DATABASE_URI = ''
+SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 '''
