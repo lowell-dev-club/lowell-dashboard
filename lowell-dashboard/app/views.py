fce5b43b6c147a9203c0a8bdd0e5775f3715f0bf
calee14 <caplee1000@gmail.com>
2019-05-30 18:00:34 -0700

    adding route for viewing users posts

--- lowell-dashboard/app/views.py
+++ lowell-dashboard/app/views.py
@@ -153,3 +153,15 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    '''
+    Grabs all posts from the user
+    Maybe in the future have it look to their account page
+    '''
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
+
