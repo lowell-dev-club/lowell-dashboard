ea560271de02bd0662948eff7dff57e220364489
Savagecoder77 <savagecoder77@gmail.com>
2019-06-03 15:35:01 -0700

    new send email function

---
+++ lowell-dashboard/app/email.py
@@ -0,0 +1,16 @@
+from project import app, mail
+from flask_mail import Message
+
+'''
+Function to send any emails 
+for Lowell Dashboard
+'''
+
+def send_email(to, subject, template):
+    msg = Message(
+        subject,
+        recipients=[to],
+        html=template,
+        sender=app.config['MAIL_DEFAULT_SENDER']
+    )
+    mail.send(msg)
