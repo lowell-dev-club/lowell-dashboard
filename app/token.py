51cdc38463108fa9a4ebd2eb6633be6b52dbf3d9
Savagecoder77 <savagecoder77@gmail.com>
2020-02-06 13:32:29 -0800

    pep 8

--- app/token.py
+++ app/token.py
@@ -16,6 +16,6 @@ def confirm_token(token, expiration=3600):
             salt=app.config['SECURITY_PASSWORD_SALT'],
             max_age=expiration
         )
-    except:
+    except BaseException:
         return False
     return email
